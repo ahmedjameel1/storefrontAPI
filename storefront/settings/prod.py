88e2190b106b459c3b6093af77d2d768426a8365
root <root@DESKTOP-R9FOI17>
2024-05-26 21:35:54 +0300

    cd

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -18,3 +18,16 @@ DATABASES = {
         conn_max_age=600
     )
 }
+
+CELERY_BROKER_URL = os.environ.get('redis-cache-broker')
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get('redis-cache-broker'),
+        "TIMEOUT": 10*60,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
